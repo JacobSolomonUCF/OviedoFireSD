d8b1f17fc0c38f181c98b6743d63d4a56927619a
cjaen <chris.jaen@gmail.com>
2017-08-30 19:31:07 -0400

    updated front-end

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 
+import {AngularFireAuth} from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {WebService} from "./services/webService";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +12,21 @@ import {Component, ViewEncapsulation} from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-	editList;
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth, public webService: WebService) {
+    this.user = this.afAuth.authState;
+  }
+
+  checkState() {
+    return this.webService.checkState();
+  }
+
+  login(email, password) {
+    this.webService.login(email, password);
+  }
+
+  logout() {
+    this.webService.logout();
+  }
 }
