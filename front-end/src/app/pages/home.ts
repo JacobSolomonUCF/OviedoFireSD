c29e1f7bef6a818f2b78ad21314472349f9d3d49
Ashton <ashton.ansag@panderasystems.com>
2017-11-03 02:32:58 -0400

    Added failed warning section.

--- front-end/src/app/pages/home.ts
+++ front-end/src/app/pages/home.ts
@@ -30,101 +30,117 @@ import {WebService} from "../services/webService";
 					</div>
 				</div>
 				<br>
-				<div class="row flex">
-					<div class="alert alert-danger tile flex-grow"
-							 *ngIf="alerts && alerts.repairItems && alerts.repairItems.count">
-						<div class="tile-head">
-							<h3 class="pure-u-4-5">Repairs Needed</h3>
-						</div>
-						<div class="centered" *ngIf="loading">
-							<br/>
-							<i class="fa fa-5x fa-spinner fa-pulse"></i>
-						</div>
-						<ul class="to-do">
-							<li *ngFor="let x of alerts.repairItems.properties">
-								<div>
-									<label class="pure-checkbox" *ngIf="alerts.repairItems[x]">
-										<i class="fa fa-lg fa-close" (click)="dismiss('repairItems', x)"></i>
-										{{alerts.repairItems[x]}}
-									</label>
-								</div>
-							</li>
-						</ul>
+				<div class="alert alert-danger tile flex-grow"
+						 *ngIf="alerts && alerts.repairItems && alerts.repairItems.count">
+					<div class="tile-head">
+						<h3 class="pure-u-4-5">Repairs Needed</h3>
 					</div>
+					<div class="centered" *ngIf="loading">
+						<br/>
+						<i class="fa fa-5x fa-spinner fa-pulse"></i>
+					</div>
+					<ul class="to-do">
+						<li *ngFor="let x of alerts.repairItems.properties">
+							<div>
+								<label class="pure-checkbox" *ngIf="alerts.repairItems[x]">
+									<i class="fa fa-lg fa-close" (click)="dismiss('repairItems', x)"></i>
+									{{alerts.repairItems[x]}}
+								</label>
+							</div>
+						</li>
+					</ul>
 				</div>
-				<div class="row flex wrap">
-					<div class="alert alert-info tile flex-grow max2"
-							 *ngIf="alerts && alerts.missingItems && alerts.missingItems.count">
+				<div class="alert alert-info tile flex-grow max2"
+						 *ngIf="alerts && alerts.missingItems && alerts.missingItems.count">
+					<div class="tile-head">
+						<h3 class="pure-u-4-5">Missing Equipment</h3>
+					</div>
+					<div class="centered" *ngIf="loading">
+						<br/>
+						<i class="fa fa-5x fa-spinner fa-pulse"></i>
+					</div>
+					<ul class="to-do">
+						<li *ngFor="let x of alerts.missingItems.properties">
+							<div>
+								<label class="pure-checkbox" *ngIf="alerts.missingItems[x]">
+									<i class="fa fa-lg fa-close" (click)="dismiss('missingItems', x)"></i>
+									{{alerts.missingItems[x]}}
+								</label>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="alert alert-warning tile flex-grow max2"
+						 *ngIf="alerts && alerts.incompleteForms && alerts.incompleteForms.count">
+					<div class="tile-head">
+						<h3 class="pure-u-4-5">Forgotten Reports</h3>
+					</div>
+					<div class="centered" *ngIf="loading">
+						<br/>
+						<i class="fa fa-5x fa-spinner fa-pulse"></i>
+					</div>
+					<ul class="to-do">
+						<li *ngFor="let x of alerts.incompleteForms.properties">
+							<div>
+								<label class="pure-checkbox" *ngIf="alerts.incompleteForms[x]">
+									<i class="fa fa-lg fa-close" (click)="dismiss('incompleteForms', x)"></i>
+									{{alerts.incompleteForms[x]}}
+								</label>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="alert alert-danger tile flex-grow"
+						 *ngIf="alerts && alerts.failItems && alerts.failItems.count">
 						<div class="tile-head">
-							<h3 class="pure-u-4-5">Missing Equipment</h3>
+							<h3 class="pure-u-4-5">Failed Items</h3>
 						</div>
 						<div class="centered" *ngIf="loading">
 							<br/>
 							<i class="fa fa-5x fa-spinner fa-pulse"></i>
 						</div>
-						<ul class="to-do">
-							<li *ngFor="let x of alerts.missingItems.properties">
-								<div>
-									<label class="pure-checkbox" *ngIf="alerts.missingItems[x]">
-										<i class="fa fa-lg fa-close" (click)="dismiss('missingItems', x)"></i>
-										{{alerts.missingItems[x]}}
+					<ul class="to-do">
+						<li *ngFor="let x of alerts.failItems.properties">
+							<div>
+								<label class="pure-checkbox" *ngIf="alerts.failItems[x]">
+									<i class="fa fa-lg fa-close" (click)="dismiss('failItems', x)"></i>
+									{{alerts.failItems[x]}}
 									</label>
 								</div>
 							</li>
 						</ul>
 					</div>
-					<div class="alert alert-warning tile flex-grow max2"
-							 *ngIf="alerts && alerts.incompleteForms && alerts.incompleteForms.count">
-						<div class="tile-head">
-							<h3 class="pure-u-4-5">Forgotten Reports</h3>
-						</div>
-						<div class="centered" *ngIf="loading">
-							<br/>
-							<i class="fa fa-5x fa-spinner fa-pulse"></i>
-						</div>
-						<ul class="to-do">
-							<li *ngFor="let x of alerts.incompleteForms.properties">
-								<div>
-									<label class="pure-checkbox" *ngIf="alerts.incompleteForms[x]">
-										<i class="fa fa-lg fa-close" (click)="dismiss('incompleteForms', x)"></i>
-										{{alerts.incompleteForms[x]}}
-									</label>
-								</div>
+				<div class="tile white flex-grow max2">
+					<div class="tile-head">
+						<h3 class="pure-u-4-5">To Do List</h3>
+						<ul class="pure-u-1-5 options" hidden>
+							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+									 aria-expanded="false"><i
+									class="fa fa-wrench"></i></a>
+								<ul class="dropdown-menu" role="menu">
+								</ul>
+							</li>
+							<li><a class="close-link"><i class="fa fa-close"></i></a>
 							</li>
 						</ul>
 					</div>
-					<div class="tile white flex-grow max2">
-						<div class="tile-head">
-							<h3 class="pure-u-4-5">To Do List</h3>
-							<ul class="pure-u-1-5 options" hidden>
-								<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-								</li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-										 aria-expanded="false"><i
-										class="fa fa-wrench"></i></a>
-									<ul class="dropdown-menu" role="menu">
-									</ul>
-								</li>
-								<li><a class="close-link"><i class="fa fa-close"></i></a>
-								</li>
-							</ul>
-						</div>
-						<div class="centered" *ngIf="loading">
-							<br/>
-							<i class="fa fa-5x fa-spinner fa-pulse"></i>
-						</div>
-						<ul class="to-do white">
-							<li *ngFor="let todo of toDoList; let i = index">
-								<div class="white">
-									<label class="pure-checkbox">
-										<i class="fa fa-lg fa-square-o white"></i>
-										{{todo.title}}
-									</label>
-								</div>
-							</li>
-						</ul>
+					<div class="centered" *ngIf="loading">
+						<br/>
+						<i class="fa fa-5x fa-spinner fa-pulse"></i>
 					</div>
+					<ul class="to-do white">
+						<li *ngFor="let todo of toDoList; let i = index">
+							<div class="white">
+								<label class="pure-checkbox">
+									<i class="fa fa-lg fa-square-o white"></i>
+									{{todo.title}}
+								</label>
+							</div>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</div>`
@@ -145,8 +161,9 @@ export class Home {
 			.getHome()
 			.subscribe(resp => {
 					this.alerts = resp['alerts'];
+					console.log(resp['alerts']);
 					if (this.alerts) {
-						['incompleteForms', 'missingItems', 'repairItems'].map(prop => {
+						['incompleteForms', 'missingItems', 'repairItems', 'failItems'].map(prop => {
 							if (this['alerts'][prop]) {
 								let keys = Object.keys(this['alerts'][prop]);
 								this['alerts'][prop].properties = keys;
