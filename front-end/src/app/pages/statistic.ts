7cd3d6878720765d1bb38c3f1ee498e683a1d7bd
Ashton <ashton.ansag@panderasystems.com>
2017-11-13 01:30:04 -0500

    Finish stats section

--- front-end/src/app/pages/statistic.ts
+++ front-end/src/app/pages/statistic.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, ViewChild} from "@angular/core";
 import {WebService} from "../services/webService";
 
 @Component({
@@ -12,7 +12,15 @@ import {WebService} from "../services/webService";
 				<i class="fa fa-5x fa-spinner fa-pulse"></i>
 			</div>
 			<div *ngSwitchCase="false">
-				<canvas baseChart
+				<select [(ngModel)]="index" (change)="switchTo()">
+					<option *ngFor="let data of original; index as i" [value]="i">{{data.title}}</option>
+				</select>
+				<select [(ngModel)]="level" (change)="switchTo()">
+					<option *ngFor="let time of original[index].details; index as i" [value]="i">{{time}}</option>
+				</select>
+
+				<canvas #myChart
+								baseChart
 								[datasets]="barChartData"
 								[labels]="barChartLabels"
 								[options]="barChartOptions"
@@ -25,35 +33,59 @@ import {WebService} from "../services/webService";
 })
 export class Statistic {
 
+	@ViewChild('myChart') myChart: ElementRef;
+
 	loading = true;
 	public barChartOptions: any = {
 		scaleShowVerticalLines: false,
-		responsive: true
+		responsive: true,
+		scales: {
+			yAxes: [{
+				display: true,
+				ticks: {
+					beginAtZero: true,
+					suggestedMax: 10
+				}
+			}]
+		}
 	};
-	public barChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+	public barChartLabels: string[] = [];
 	public barChartType: string = 'bar';
 	public barChartLegend: boolean = true;
-
+	public index: number = 0;
+	public level: number = 0;
+	public original: any[];
 	public barChartData: any[] = [];
 
 	constructor(webService: WebService) {
 		webService.setState('statistics')
 			.doGet('/statistics')
 			.subscribe(resp => {
-				this.barChartLabels = Object.keys(resp);
-				let repairsNeeded = [], missing = [], failed = [];
-				this.barChartLabels.map(label => {
-					repairsNeeded.push(resp[label].repairsNeeded);
-					missing.push(resp[label].missing);
-					failed.push(resp[label].failed);
-				});
-				this.barChartData.push({label: 'Needed Repair', data: repairsNeeded});
-				this.barChartData.push({label: 'Missing', data: missing});
-				this.barChartData.push({label: 'Failed', data: failed});
+
+				let result = {...resp['results'][0][resp['results'][0].details[0]]};
+				this.original = resp['results'];
+				this.barChartLabels = [...result.labels];
+				if (!result.data) result.data = this.barChartLabels.map(label => { return {label: label, data:[0]}});
+				this.barChartData = [...result.data];
 			}, () => {
+				this.loading = false;
 			}, () => {
 				this.loading = false;
 			})
 		;
+	}
+
+	switchTo(retry = false) {
+		this.barChartLabels.length = 0;
+		let time = this.original[this.index].details[this.level];
+		if (!time) {
+			this.level = 0;
+			return retry ? false : this.switchTo(true);
+		}
+		let data = this.original[this.index][time];
+		data.labels.forEach(label => {
+			this.barChartLabels.push(label);
+		});
+		this.barChartData = data.data;
 	}
 }
